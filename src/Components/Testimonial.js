7313a5ec487a6ab302ded5555fe78c0f6f9a817c
shridhar <shridharshet2101@gmail.com>
2023-06-13 10:00:27 +0530

    Sample commit

---
+++ src/Components/Testimonial.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ProfilePic from "../Assets/john-doe-image.png";
+import { AiFillStar } from "react-icons/ai";
+
+const Testimonial = () => {
+  return (
+    <div className="work-section-wrapper">
+      <div className="work-section-top">
+        <p className="primary-subheading">Testimonial</p>
+        <h1 className="primary-heading">What They Are Saying</h1>
+        <p className="primary-text">
+        Lorem ipsum dolor sit amet consectetur. Et elit non magna tincidunt. Fringilla quam magnis molestie turpis dui facilisis at. Consectetur ipsum dolor sit amet non tincidunt. Elit fringilla quam magna facilisis at dui molestie turpis. Magnis consectetur dolor sit amet facilisis non tincidunt elit. Tincidunt magna dolor sit amet fringilla facilisis at dui turpis.
+        </p>
+      </div>
+      <div className="testimonial-section-bottom">
+        <img src={ProfilePic} alt="" />
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        </p>
+        <div className="testimonials-stars-container">
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+          <AiFillStar />
+        </div>
+        <h2>John Doe</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
